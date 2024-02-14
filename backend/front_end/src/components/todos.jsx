41e21f441b7d27e3cb6bb94f41d9235a148a836e
akshatasarwade <aa.sarwade19@gmail.com>
2024-02-14 21:56:37 +0530

    files

---
+++ backend/front_end/src/components/todos.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
+
+/* todos = [
+   {
+    title: "go to gym",
+    description: "go to gym",
+   } 
+   ]
+*/
+export function Todos({todos}) {
+
+    return <div>
+        {todos.map(function(todo) {
+            return <div>
+                <h1>{todo.title}</h1>
+                <h2>{todo.description}</h2>
+                <button>{todo.completed == true ? "Completed" : "Mark as Complete"}</button>
+            </div>
+        })}
+    </div>
+}
